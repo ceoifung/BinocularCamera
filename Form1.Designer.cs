81ee6c90a796b5e4c25c1784d65bd6f826620785
kylin <33998180+ceoifung@users.noreply.github.com>
2021-11-30 11:47:41 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace BinocularCamera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.CameraToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.CameraToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.RefreshDeviceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.videoSourcePlayer2 = new AForge.Controls.VideoSourcePlayer();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CameraToolStripMenuItem1,
+            this.CameraToolStripMenuItem2,
+            this.RefreshDeviceToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(882, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // CameraToolStripMenuItem1
+            // 
+            this.CameraToolStripMenuItem1.Name = "CameraToolStripMenuItem1";
+            this.CameraToolStripMenuItem1.Size = new System.Drawing.Size(77, 24);
+            this.CameraToolStripMenuItem1.Text = "摄像头1";
+            // 
+            // CameraToolStripMenuItem2
+            // 
+            this.CameraToolStripMenuItem2.Name = "CameraToolStripMenuItem2";
+            this.CameraToolStripMenuItem2.Size = new System.Drawing.Size(77, 24);
+            this.CameraToolStripMenuItem2.Text = "摄像头2";
+            // 
+            // RefreshDeviceToolStripMenuItem
+            // 
+            this.RefreshDeviceToolStripMenuItem.Name = "RefreshDeviceToolStripMenuItem";
+            this.RefreshDeviceToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.RefreshDeviceToolStripMenuItem.Text = "刷新设备";
+            this.RefreshDeviceToolStripMenuItem.Click += new System.EventHandler(this.RefreshDeviceToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.videoSourcePlayer1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(440, 397);
+            this.panel1.TabIndex = 1;
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.BorderColor = System.Drawing.Color.DimGray;
+            this.videoSourcePlayer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(0, 0);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(440, 397);
+            this.videoSourcePlayer1.TabIndex = 0;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            this.videoSourcePlayer1.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer1_NewFrame);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.videoSourcePlayer2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(442, 28);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(440, 397);
+            this.panel2.TabIndex = 2;
+            // 
+            // videoSourcePlayer2
+            // 
+            this.videoSourcePlayer2.BorderColor = System.Drawing.Color.DimGray;
+            this.videoSourcePlayer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.videoSourcePlayer2.Location = new System.Drawing.Point(0, 0);
+            this.videoSourcePlayer2.Name = "videoSourcePlayer2";
+            this.videoSourcePlayer2.Size = new System.Drawing.Size(440, 397);
+            this.videoSourcePlayer2.TabIndex = 1;
+            this.videoSourcePlayer2.Text = "videoSourcePlayer2";
+            this.videoSourcePlayer2.VideoSource = null;
+            this.videoSourcePlayer2.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer2_NewFrame);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 425);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "双目摄像头测试软件 | Copyright © 2021 | 饮尽鸿爪下的雪泥 · 小R科技强力驱动";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem CameraToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem CameraToolStripMenuItem2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer2;
+        private System.Windows.Forms.ToolStripMenuItem RefreshDeviceToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using AForge.Video.DirectShow;
+using BinocularCamera.Properties;
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace BinocularCamera
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevices;//所有摄像设备
+        private VideoCaptureDevice videoDevice1;//摄像设备
+        private VideoCaptureDevice videoDevice2;//摄像设备
+        private ToolStripMenuItem preCameraItem1 = null;
+        private ToolStripMenuItem preCameraItem2 = null;
+        //private VideoCapabilities[] videoCapabilities;//摄像头分辨率
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetVideoLstTbl();
+        }
+
+        /// <summary>
+        /// 获取视频设备
+        /// </summary>
+        private void GetVideoLstTbl()
+        {
+            CameraToolStripMenuItem1.DropDownItems.Clear();
+            CameraToolStripMenuItem2.DropDownItems.Clear();
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);//得到机器所有接入的摄像设备
+            if (videoDevices.Count != 0)
+            {
+                foreach (FilterInfo device in videoDevices)
+                {
+                    CameraToolStripMenuItem1.DropDownItems.Add(device.Name, null, new EventHandler(MenuCamera1Clicked));
+                    CameraToolStripMenuItem2.DropDownItems.Add(device.Name, null, new EventHandler(MenuCamera2Clicked));
+                }
+                if (videoDevices.Count >= 2)
+                {
+                    CameraToolStripMenuItem1.DropDownItems[0].PerformClick();
+                    CameraToolStripMenuItem2.DropDownItems[1].PerformClick();
+                }
+                else
+                {
+                    CameraToolStripMenuItem1.DropDownItems[0].PerformClick();
+                }
+
+            }
+            else
+            {
+                CameraToolStripMenuItem1.DropDownItems.Add("没有找到摄像头");
+                CameraToolStripMenuItem2.DropDownItems.Add("没有找到摄像头");
+            }
+        }
+
+        private void MenuCamera2Clicked(object sender, EventArgs e)
+        {
+            if (preCameraItem2 != (sender as ToolStripMenuItem))
+            {
+                if (preCameraItem2 != null)
+                    preCameraItem2.Image = null;
+                (sender as ToolStripMenuItem).Image = Resources._checked;
+                preCameraItem2 = (sender as ToolStripMenuItem);
+                DisConnect(2);
+                videoDevice2 = new VideoCaptureDevice(videoDevices[CameraToolStripMenuItem2.DropDownItems.IndexOf(preCameraItem2)].MonikerString);
+                //videoCapabilities = videoDevice2.VideoCapabilities;
+                //videoDevice2.VideoResolution = videoCapabilities[0]; //摄像头分辨率
+                videoSourcePlayer2.VideoSource = videoDevice2;//把摄像头赋给控件
+                videoSourcePlayer2.Start();//开启摄像头
+            }
+        }
+
+        private void MenuCamera1Clicked(object sender, EventArgs e)
+        {
+            if (preCameraItem1 != (sender as ToolStripMenuItem))
+            {
+                if (preCameraItem1 != null)
+                    preCameraItem1.Image = null;
+                (sender as ToolStripMenuItem).Image = Resources._checked;
+                preCameraItem1 = (sender as ToolStripMenuItem);
+                DisConnect(1);
+                videoDevice1 = new VideoCaptureDevice(videoDevices[CameraToolStripMenuItem1.DropDownItems.IndexOf(preCameraItem1)].MonikerString);
+                videoSourcePlayer1.VideoSource = videoDevice1;//把摄像头赋给控件
+                videoSourcePlayer1.Start();//开启摄像头
+            }
+        }
+
+        /// <summary>
+        /// 关闭并释放
+        /// </summary>
+        private void DisConnect(int type)
+        {
+            if (type == 1)
+            {
+                if (videoSourcePlayer1.VideoSource != null)
+                {
+                    videoSourcePlayer1.SignalToStop();
+                    videoSourcePlayer1.WaitForStop();
+                    videoSourcePlayer1.VideoSource = null;
+                }
+            }
+            else
+            {
+                if (videoSourcePlayer2.VideoSource != null)
+                {
+                    videoSourcePlayer2.SignalToStop();
+                    videoSourcePlayer2.WaitForStop();
+                    videoSourcePlayer2.VideoSource = null;
+                }
+            }
+            
+        }
+
+        private void DisConnectAll()
+        {
+            if (videoSourcePlayer1.VideoSource != null)
+            {
+                videoSourcePlayer1.SignalToStop();
+                videoSourcePlayer1.WaitForStop();
+                videoSourcePlayer1.VideoSource = null;
+            }
+            if (videoSourcePlayer2.VideoSource != null)
+            {
+                videoSourcePlayer2.SignalToStop();
+                videoSourcePlayer2.WaitForStop();
+                videoSourcePlayer2.VideoSource = null;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DisConnectAll();
+        }
+
+        private void videoSourcePlayer1_NewFrame(object sender, ref Bitmap image)
+        {
+            Graphics g = Graphics.FromImage(image);
+            Font font = new Font("Arial", 20, FontStyle.Regular, GraphicsUnit.Point);
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.DrawString("video1_Ceoifung", font, Brushes.Red, new PointF(5, 15));
+        }
+
+        private void videoSourcePlayer2_NewFrame(object sender, ref Bitmap image)
+        {
+            Graphics g = Graphics.FromImage(image);
+            Font font = new Font("Arial", 20, FontStyle.Regular, GraphicsUnit.Point);
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.DrawString("video2_Ceoifung", font, Brushes.Red, new PointF(5, 15));
+        }
+
+        private void RefreshDeviceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DisConnectAll();
+            GetVideoLstTbl();
+        }
+    }
+}
